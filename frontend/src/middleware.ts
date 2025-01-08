eb304617072441282e0d26d60f7dbfa9b3fa7ce8
abanob bostouros <abanob.bostouros@xceedcore.com>
2025-01-08 15:59:56 -0800

    Update middleware with proper auth handling

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,12 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
 
-export default clerkMiddleware();
+export default authMiddleware({
+  // Routes that can be accessed while signed out
+  publicRoutes: ['/', '/contact', '/about'],
+  // Routes that can always be accessed, and have
+  // no authentication information
+  ignoredRoutes: ['/api/webhook'],
+  afterAuth(auth, req, evt) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.+\.[\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
