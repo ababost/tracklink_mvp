26de265dfea38d895661689b4b75406cc9ba3ff2
abanob bostouros <abanob.bostouros@xceedcore.com>
2025-01-08 16:10:10 -0800

    Update middleware to handle dashboard redirects

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,17 +1,27 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
-
+import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+ 
+const publicPaths = ["/", "/sign-in*", "/sign-up*"];
+ 
+// Create route matcher to check if path is public
+const isPublic = createRouteMatcher(publicPaths);
+ 
+export default clerkMiddleware((auth, req, evt) => {
+  if (isPublic(req)) {
+    // If user is signed in and trying to access public route (like homepage)
+    // redirect them to their dashboard
+    if (auth.userId) {
+      const dashboard = new URL('/artists/dashboard', req.url);
+      return NextResponse.redirect(dashboard);
+    }
+    return NextResponse.next();
+  }
+ 
+  // If user tries to access protected route and is not signed in
+  // allow clerk to handle redirection to sign-in
+  return NextResponse.next();
+});
+ 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    "/((?!static|.*\..*|_next|favicon.ico).*)",
-    "/",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/"],
 };
